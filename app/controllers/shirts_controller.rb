705bd898ea595d89ddb8f8ad27823cdee28aebb2
Steven Nunez <snunez@cyrusinnovation.com>
2013-05-24 10:06:28 -0400

    Finish edit and update actions

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -1,4 +1,6 @@
 class ShirtsController < ApplicationController
+  before_action :set_shirt, only: [:edit, :show, :update]
+
   def index
     @shirts = Shirt.all
   end
@@ -8,13 +10,20 @@ class ShirtsController < ApplicationController
   end
 
   def create
-    safe_params = params.require('shirt').permit(:name, :description, :image)
-    @shirt = Shirt.new(safe_params)
+    @shirt = Shirt.new(shirt_params)
     if @shirt.save
       redirect_to @shirt
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @shirt.update shirt_params
+    redirect_to @shirt
   end
 
   def search
@@ -26,8 +35,27 @@ class ShirtsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def shirt_params
+    params.require('shirt').permit(:name, :description, :image)
+  end
+
+  def set_shirt
     @shirt = Shirt.find(params[:id])
-  rescue
+  rescue ActiveRecord::RecordNotFound
+    flash.now[:notice] = "Invalid Shirt ID #{params[:id]}"
     redirect_to root_path
   end
 end
+
+
+
+
+
+
+
+
+
