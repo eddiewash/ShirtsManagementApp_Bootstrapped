ae36ae7b6311f4d7fee35f1a50eb6e6591dab3ad
Eddie <edward.washington10@gmail.com>
2013-05-30 03:30:50 -0400

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,29 @@
-Shirt.create name: "Rushed to Market", description: "Meta Shirt showing Warning about inability to load shirt", image: "rushed_to_market.png"
-Shirt.create name: "Darwin's Delight", description: "Darwin with paper cutouts of evolution", image: "darwins_delight.png"
-Shirt.create name: "Victory through Chaos", description: "Button Masher's Manifesto", image: "victory_through_chaos.png"
-Shirt.create name: "Console Wars", description: "Gaming consoles as Star Wars battle vehicles", image: "console_wars.png"
-Shirt.create name: "Downtime", description: "Lightsabers charging at an outlet", image: "downtime.png"
-Shirt.create name: "Shoot First", description: "Solo and Greedo in 8-bit emulating text based game", image: "shoot_first.png"
-Shirt.create name: "I Quit!", description: "Stick Figure guy has enough", image: "i_quit.png"
-Shirt.create name: "Family Breakfast", description: "Vader makes eggs for a young Luke and Leia", image: "family_breakfast.png"
-Shirt.create name: "Music Pirate", description: "Pirate turns to digital piracy", image: "music_pirate.png"
-Shirt.create name: "Leonardo", description: "Ninja Turtle as famous painter", image: "leonardo.png"
-Shirt.create name: "My Morning Sidekick", description: "Flying cup of coffee", image: "my_morning_sidekick.png"
-Shirt.create name: "Casual Friday", description: "Storm Trooper enjoying a casual Friday", image: "casual_friday.png"
-Shirt.create name: "WET||SLIPPERY WHEN", description: "Portals used to clean spills", image: "wet_slippery_when.png"
-Shirt.create name: "Blending In", description: "Waldo Hiding from some Ninjas!", image: "blending_in.png"
-Shirt.create name: "The Binge", description: "Cookie monster had one to many at the milk bar", image: "the_binge.png"
-Shirt.create name: "Motivation", description: "Training using Bacon as a motivator", image: "motivation.png"
-Shirt.create name: "Winners at Life", description: "Phoenix, Cat, and Zombie at longevity competition", image: "winners_at_life.png"
-Shirt.create name: "Halfling Height Requirement", description: "Gandalf as a queue manager", image: "halfing_height_requirement.png"
-Shirt.create name: "The Last One", description: "Sad Polar Bear is sad", image: "the_last_one.png"
-Shirt.create name: "Panda Pirate", description: "Panda as a Pirate", image: "panda_pirate.png"
+Vendor.create name: "Competitive Edge", website: "http://jdrfteamshirts.compet.com"
+Vendor.create name: "ShadowFax Graphics", website: "http://www.shadowfaxgraphics.com/"
+Vendor.create name: "CustomInk", website: "http://www.customink.com/"
+Vendor.create name: "Evercom Printing", website: "http://www.evercom.com/"
+Vendor.create name: "Adventures in Printing", website: "http://www.adventuresinprinting.com/"
+Vendor.create name: "Extreme Tees", website: "http://www.extremeteesonline.com/"
+
+
+Shirt.create name: "Rushed to Market", description: "Meta Shirt showing Warning about inability to load shirt", image: "rushed_to_market.png", vendor_id: 1
+Shirt.create name: "Darwin's Delight", description: "Darwin with paper cutouts of evolution", image: "darwins_delight.png", vendor_id: 1
+Shirt.create name: "Victory through Chaos", description: "Button Masher's Manifesto", image: "victory_through_chaos.png", vendor_id: 1
+Shirt.create name: "Console Wars", description: "Gaming consoles as Star Wars battle vehicles", image: "console_wars.png", vendor_id: 2
+Shirt.create name: "Downtime", description: "Lightsabers charging at an outlet", image: "downtime.png", vendor_id: 2
+Shirt.create name: "Shoot First", description: "Solo and Greedo in 8-bit emulating text based game", image: "shoot_first.png", vendor_id: 2
+Shirt.create name: "I Quit!", description: "Stick Figure guy has enough", image: "i_quit.png", vendor_id: 3
+Shirt.create name: "Family Breakfast", description: "Vader makes eggs for a young Luke and Leia", image: "family_breakfast.png", vendor_id: 3
+Shirt.create name: "Music Pirate", description: "Pirate turns to digital piracy", image: "music_pirate.png", vendor_id: 3
+Shirt.create name: "Leonardo", description: "Ninja Turtle as famous painter", image: "leonardo.png", vendor_id: 4
+Shirt.create name: "My Morning Sidekick", description: "Flying cup of coffee", image: "my_morning_sidekick.png", vendor_id: 4
+Shirt.create name: "Casual Friday", description: "Storm Trooper enjoying a casual Friday", image: "casual_friday.png", vendor_id: 4
+Shirt.create name: "WET||SLIPPERY WHEN", description: "Portals used to clean spills", image: "wet_slippery_when.png", vendor_id: 5
+Shirt.create name: "Blending In", description: "Waldo Hiding from some Ninjas!", image: "blending_in.png", vendor_id: 5
+Shirt.create name: "The Binge", description: "Cookie monster had one to many at the milk bar", image: "the_binge.png", vendor_id: 5
+Shirt.create name: "Motivation", description: "Training using Bacon as a motivator", image: "motivation.png", vendor_id: 5
+Shirt.create name: "Winners at Life", description: "Phoenix, Cat, and Zombie at longevity competition", image: "winners_at_life.png", vendor_id: 6
+Shirt.create name: "Halfling Height Requirement", description: "Gandalf as a queue manager", image: "halfing_height_requirement.png", vendor_id: 6
+Shirt.create name: "The Last One", description: "Sad Polar Bear is sad", image: "the_last_one.png", vendor_id: 6
+Shirt.create name: "Panda Pirate", description: "Panda as a Pirate", image: "panda_pirate.png", vendor_id: 6
+
